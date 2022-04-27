c319f46bbce46de829e7ae868045f532d1d7ad5e
Daigo U <go26dev@gmail.com>
2022-04-27 16:21:25 +0900

    Initial Commit

---
+++ MultiModuleSample/MultiModuleSampleApp.swift
@@ -0,0 +1,14 @@
+//
+//  Created on 2022/04/27
+//
+
+import SwiftUI
+
+@main
+struct MultiModuleSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
